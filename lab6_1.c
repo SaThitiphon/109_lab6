ac3bdd5b551b6bde9c7ae2a9517e6d9a3d0cde4e
Thitipon <thitiphon.s@ku.th>
2021-03-15 18:54:09 +0700

    final touch

--- lab6_1.c
+++ lab6_1.c
@@ -5,13 +5,13 @@ int main(){
    char temp[25];
    scanf("%d",&n);
    char str[n][50];
-   for(i=0;i<n-1;i++)
+   for(i=0;i<n;i++)
    {
      scanf("%s",str[i]);
    }
-   for(i=0;i<=n;i++)
+   for(i=0;i<n;i++)
    {
-      for(j=i+1;j<=n;j++)
+      for(j=i+1;j<n;j++)
       {
          if(strcmp(str[i],str[j])>0)
          {
@@ -21,7 +21,7 @@ int main(){
          }
       }
    }
-   for(i=0;i<=n;i++)
+   for(i=0;i<n;i++)
    {
       printf("%s\n",str[i]);
    }
